02338632ecb0316795e3b5125942d6d2ae6ee9ca
BEOMZUKIM <kbj3144@yahoo.co.jp>
2019-12-13 17:21:09 +0900

    Initial Commit

---
+++ qq/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  qq
+//
+//  Created by BEOMZU KIM on 2019/12/13.
+//  Copyright © 2019 BEOMZU KIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
